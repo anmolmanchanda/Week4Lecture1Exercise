1fa2d19b72c68035c63edd698a7646bd50e89d5d
Anmol Manchanda <manchandaanmol@icloud.com>
2021-10-03 16:21:48 -0400

    Initial Commit

---
+++ Week4Lecture1Exercise.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+	
+class Teacher {
+    var name: String
+    var school: String
+    var yearsOfExp: Int
+
+
+init (name: String = "", school: String = "", yearsOfExp:Int = 0){
+    self.name = name
+    self.school = school
+    self.yearsOfExp = yearsOfExp
+}
+    
+}
+
+var t:Teacher = Teacher(name:"Pshemek", school:"Conestoga", yearsOfExp:20)
+print (t.name, t.school, t.yearsOfExp)
